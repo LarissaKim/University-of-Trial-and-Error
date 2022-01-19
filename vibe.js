e66203e39e47ae2851187f2713f693543cb6c965
Larissa Kim <lrsslsrk@gmail.com>
2022-01-19 13:23:14 -0700

    Add messages for client-side rendering

---
+++ vibe.js
@@ -0,0 +1,10 @@
+const vibes = [
+  ' and today will be a Jamstastic day!',
+  ' and all you need is within you right now!',
+  ' and an unstoppable force of nature!',
+  ' and so is this rabbit! 🐇',
+];
+
+const vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+document.querySelector('.vibe').append(vibe);
