35ed23dab78c58d64ba41af3526cce7b8ebe6148
Larissa Kim <lrsslsrk@gmail.com>
2022-02-22 13:08:01 -0700

    Second attempt to fix gh page deploy

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function (config) {
   config.addPassthroughCopy('src/js');
-  config.addPassthroughCopy('./src/css');
+  config.addPassthroughCopy('src/css');
 
   return {
     pathPrefix: '/jamstastic/',
     dir: {
       input: 'src',
       includes: '_includes',
-      output: '_site',
+      output: 'build',
     },
   };
 };
